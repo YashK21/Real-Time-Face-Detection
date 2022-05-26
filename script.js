f1cf5b9039d5cc3505b10d8d4efdc2a9f2c0748a
yashk21 <yashk8119@gmail.com>
2022-05-27 03:35:28 +0530

    video started

---
+++ script.js
@@ -0,0 +1,11 @@
+const video = document.getElementById("video");
+const startVideo = () => {
+  navigator.getUserMedia(
+    {
+      video: {},
+    },
+    (stream) => (video.srcObject = stream),
+    (err) => console.log(err)
+  );
+};
+startVideo();

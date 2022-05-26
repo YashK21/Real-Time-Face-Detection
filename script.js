58ed95db530899b8c8eee114170db2be463331f4
yashk21 <yashk8119@gmail.com>
2022-05-27 03:48:50 +0530

    models added and unsual files deleted

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const video = document.getElementById("video");
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+]);
 const startVideo = () => {
   navigator.getUserMedia(
     {
